5a02394de932800b576a794b765ff85bd3feb1e8
Tanzina Rahman Mona <77913975+TanzinaRahman@users.noreply.github.com>
2021-07-31 23:48:23 +0600

    Create PythonProgram50.py

---
+++ PythonProgram50.py
@@ -0,0 +1,12 @@
+#Reading a File
+
+file = open("student.txt", "r+")
+print(file.writable())
+file.close()
+
+#Text file
+
+Tanzina Rahman - student of CSE
+Ranzina Rahman - student of SWE
+Eanzina Rahman - student of BBA
+Qanzina Rahman - student of IT
